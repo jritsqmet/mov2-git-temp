8bde7dcb125bb42ce3d87c73b258b354bef85705
sabinevela <lukehemmingsteamomucho2005@gmail.com>
2024-12-31 08:29:37 -0500

    sabinevela

---
+++ screens/ScreenSV.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function ScreenSV() {
+  return (
+    <View>
+      <Text>ScreenSV</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
